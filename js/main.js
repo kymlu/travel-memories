f3d9f9b59410f58b86e035ef4fe05456fff43fcf
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2023-04-25 05:57:35 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ let isPopupVisible = false;
 function changePopupVisibility(){
   isPopupVisible = !isPopupVisible;
   document.getElementById("popup").style.visibility = isPopupVisible ? "visible" : "hidden";
+  document.getElementById("popup-background").style.visibility = isPopupVisible ? "visible" : "hidden";
 }
+
+document.getElementById("popup-background").addEventListener("click", changePopupVisibility);
+document.getElementById("info-button").addEventListener("click", changePopupVisibility);
