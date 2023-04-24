937a362b5a8e3e75ce9b2543e4ad81d2ae5d5ff4
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2023-04-24 10:18:42 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ let isPopupVisible = false;
 
 function changePopupVisibility(){
   isPopupVisible = !isPopupVisible;
-  document.getElementById("popup").isVisible = isPopupVisible; // Get the HTML template
+  document.getElementById("popup").style.visibility = isPopupVisible ? "visible" : "none"; // Get the HTML template
 }
