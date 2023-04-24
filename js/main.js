c5056cf7363a8ce69da8f508833a080651841a08
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2023-04-25 06:25:20 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ function changePopupVisibility(){
 document.getElementById("popup-close-btn").addEventListener("click", changePopupVisibility);
 document.getElementById("popup-background").addEventListener("click", changePopupVisibility);
 document.getElementById("info-button").addEventListener("click", changePopupVisibility);
+
+document.addEventListener('keydown', function(event) {
+  if(event.keyCode == 27 && isPopupVisible) {
+    changePopupVisibility();
+  }
+})
