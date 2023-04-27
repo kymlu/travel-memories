91cf56f07a02f7210a32bb824a4b480450aa09b7
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2023-04-27 20:28:09 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ function changeRegion(newRegion){
   selectedRegion = newRegion;
 }
 
-<!--document.getElementById("Shiga").style.fill = "green";document.getElementById("Shiga").style.stroke = "orange";-->
+document.getElementById("#Shiga").style.fill = "green";
+document.getElementById("#Shiga").style.stroke = "orange";
+// Create a clone of element with id ddl_1:
+let clone = document.getElementById('#Shiga').cloneNode( true ); 
+
+// Change the id attribute of the newly created element:
+clone.setAttribute( 'id', "shiga2" );
+
+clone.setAttribute("viewBox", "0 0 500 300"); 
+
+// Append the newly created element on element p 
+document.getElementById('body').insertAfter( clone );-->
 
 <!-- Popup -->
 
