d1e9c1cb9f219396345628ca33812e90c4fd9aca
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2023-05-01 13:25:23 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -210,20 +210,25 @@ function main(){
 	  }
 	})
   
-	document.getElementById("filter-food").addEventListener("mouseover", function(event) {console.log("food"); document.getElementById("filter-food-txt").style.display = "inline";});
-	document.getElementById("filter-food").addEventListener("click", function(event) {console.log("food2"); changeGalleryFilter("filter-food");});
+	document.getElementById("filter-food").addEventListener("mouseover", function(event) {document.getElementById("filter-food-txt").style.display = "inline";});
+	document.getElementById("filter-food").addEventListener("mouseout", function(event) {if(currentFilter != "filter-food"){document.getElementById("filter-food-txt").style.display = "none";}});
+	document.getElementById("filter-food").addEventListener("click", function(event) {changeGalleryFilter("filter-food");});
 	
-	document.getElementById("filter-nat").addEventListener("mouseover", function(event) {console.log("nature"); document.getElementById("filter-nat-txt").style.display = "inline";});
-	document.getElementById("filter-nat").addEventListener("click", function(event) {console.log("nature2"); changeGalleryFilter("filter-nat");});
+	document.getElementById("filter-nat").addEventListener("mouseover", function(event) {document.getElementById("filter-nat-txt").style.display = "inline";});
+	document.getElementById("filter-nat").addEventListener("mouseout", function(event) {if(currentFilter != "filter-nat"){document.getElementById("filter-nat-txt").style.display = "none";}});
+	document.getElementById("filter-nat").addEventListener("click", function(event) {changeGalleryFilter("filter-nat");});
 	
-	document.getElementById("filter-art").addEventListener("mouseover", function(event) {console.log("art"); document.getElementById("filter-art-txt").style.display = "inline";});
-	document.getElementById("filter-art").addEventListener("click", function(event) {console.log("art2"); changeGalleryFilter("filter-art");});
+	document.getElementById("filter-art").addEventListener("mouseover", function(event) {document.getElementById("filter-art-txt").style.display = "inline";});
+	document.getElementById("filter-art").addEventListener("mouseout", function(event) {if(currentFilter != "filter-art"){document.getElementById("filter-art-txt").style.display = "none";}});
+	document.getElementById("filter-art").addEventListener("click", function(event) {changeGalleryFilter("filter-art");});
 	
-	document.getElementById("filter-attr").addEventListener("mouseover", function(event) {console.log("attractions"); document.getElementById("filter-attr-txt").style.display = "inline";});
-	document.getElementById("filter-attr").addEventListener("click", function(event) {console.log("attractions2"); changeGalleryFilter("filter-attr");});
+	document.getElementById("filter-attr").addEventListener("mouseover", function(event) {document.getElementById("filter-attr-txt").style.display = "inline";});
+	document.getElementById("filter-attr").addEventListener("mouseout", function(event) {if(currentFilter != "filter-attr"){document.getElementById("filter-attr-txt").style.display = "none";}});
+	document.getElementById("filter-attr").addEventListener("click", function(event) {changeGalleryFilter("filter-attr");});
 	
-	document.getElementById("filter-myst").addEventListener("mouseover", function(event) {console.log("mystery"); document.getElementById("filter-myst-txt").style.display = "inline";});
-	document.getElementById("filter-myst").addEventListener("click", function(event) {console.log("mystery2"); changeGalleryFilter("filter-myst");});
+	document.getElementById("filter-myst").addEventListener("mouseover", function(event) {document.getElementById("filter-myst-txt").style.display = "inline";});
+	document.getElementById("filter-myst").addEventListener("mouseout", function(event) {if(currentFilter != "filter-myst"){document.getElementById("filter-myst-txt").style.display = "none";}});
+	document.getElementById("filter-myst").addEventListener("click", function(event) {changeGalleryFilter("filter-myst");});
 }
 
   
