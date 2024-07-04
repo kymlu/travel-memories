1c2c6f0e6f2e34732747c05360367bd8c30d4d73
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2024-07-04 14:44:59 -0600

    Added constants

---
+++ js/constants.js
@@ -0,0 +1,55 @@
+const LOAD_ANIMATION_TIME = 1500;
+const DEFAULT_TIMEOUT = 500;
+const SCROLL_THRESHOLD = 100;
+
+const JAPAN = "japan";
+const TAIWAN = "taiwan";
+const AUSTRALIA = "australia";
+const NEW_ZEALAND = "newzealand";
+
+const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const DAY_NAMES_EN = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const DAY_NAMES_JP = ["日", "月", "火", "水", "木", "金", "土"];
+
+const TAGS = [
+	{
+		"id": "animal",
+		"english_name": "Animals",
+		"japanese_name": "動物",
+	},
+	{
+		"id": "attractions",
+		"english_name": "Attractions",
+		"japanese_name": "観光地",
+	},
+	{
+		"id": "art",
+		"english_name": "Art",
+		"japanese_name": "美術",
+	},
+	{
+		"id": "event",
+		"english_name": "Events",
+		"japanese_name": "イベント",
+	},
+	{
+		"id": "food",
+		"english_name": "Food",
+		"japanese_name": "食べ物",
+	},
+	{
+		"id": "nature",
+		"english_name": "Nature",
+		"japanese_name": "自然",
+	},
+	{
+		"id": "relax",
+		"english_name": "Daily life",
+		"japanese_name": "日常",
+	},
+	{
+		"id": "town",
+		"english_name": "Around town",
+		"japanese_name": "街中で",
+	}
+];
